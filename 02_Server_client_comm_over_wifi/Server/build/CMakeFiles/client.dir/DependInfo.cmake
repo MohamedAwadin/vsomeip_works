8e748e44d676d92b0c9116ddbac78228ce98fc61
MohamedAwadin <mohamedawadin01@gmail.com>
2025-06-18 21:19:23 +0300

    add last practise

---
+++ 02_Server_client_comm_over_wifi/Server/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/awadin/ITI_9Monthes/someip/server_client_comm_over_wifi/Server/client_main.cpp" "CMakeFiles/client.dir/client_main.cpp.o" "gcc" "CMakeFiles/client.dir/client_main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
